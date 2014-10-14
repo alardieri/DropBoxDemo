65c7aab42f6125fcf15bcb4ae5692e52cf80aaae
Adam Lardieri <alardieri@AdamL-MBAir11-tag174.local>
2014-10-13 20:46:31 -0700

    Everything but Milestone 6
    Dropbox HW Week 1

---
+++ DropboxHWweek1/CreateFiveViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CreateFiveViewController.swift
+//  DropboxHWweek1
+//
+//  Created by Adam Lardieri on 10/13/14.
+//  Copyright (c) 2014 Adam Lardieri. All rights reserved.
+//
+
+import UIKit
+
+class CreateFiveViewController: UIViewController {
+
+    @IBAction func onTapButton(sender: AnyObject) {
+    navigationController?.popToRootViewControllerAnimated(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DropboxHWweek1/SignInThreeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignInThreeViewController.swift
+//  DropboxHWweek1
+//
+//  Created by Adam Lardieri on 10/13/14.
+//  Copyright (c) 2014 Adam Lardieri. All rights reserved.
+//
+
+import UIKit
+
+class SignInThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
